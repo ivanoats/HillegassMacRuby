9b8697fa4ade6ccd9bc258d48110188d645a6136
Daniel Lopes <danielvlopes@gmail.com>
2010-05-05 22:06:39 -0300

    finishing chapter 12

--- CH12/RaiseMan/AppController.rb
+++ CH12/RaiseMan/AppController.rb
@@ -5,11 +5,18 @@
 # Copyright 2010 area criacoes. All rights reserved.
 class AppController
 	
+	attr_accessor :preferenceController, :aboutPanel
+	
 	def showPreferencePanel(sender)
 		@preferenceController = PreferenceController.new
 		@preferenceController.showWindow(self)
 		NSLog("showing #{@preferenceController}")		
 	end	
 	
+	def showAboutPanel(sender)
+		successful = NSBundle.loadNibNamed("About", owner:self) if @aboutPanel.nil?
+		puts "ABOUT"
+		@aboutPanel.makeKeyAndOrderFront(nil)
+	end
+	
 end
-
